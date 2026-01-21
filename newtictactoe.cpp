677f89402a7682da75ee94a1ef8a6efa83030c26
Asya Yayla <149153342+Asyayla@users.noreply.github.com>
2026-01-21 22:07:50 +0300

    Refactor Tic Tac Toe game logic and functions

--- newtictactoe.cpp
+++ newtictactoe.cpp
@@ -1,193 +1,160 @@
 #include <iostream>
 using namespace std;
-bool checkwinner(char*, char, char);
-bool checktie(char*);
-void playermove(char*, char);
-void board(char*);
-void aimove(char*, char);
-void cleanboard(char*);
-int main () {
-     char spaces[9]={' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-     char player='X';
-     char ai='O';
-     char again;
-     bool running=true;
-     while(running) {
-          playermove(spaces, player);
-          board(spaces);
-          if(checkwinner(spaces, player, ai)) {
-               cout << "\n\nDo you want to play again?(Y-Yes N-No)\n";
-               cin >> again;
-               if(again=='n' || again=='N') {
-                    running=false;
-                    break;
-               }
-               else{
-                    cleanboard(spaces);
-                    running=true;
-               }
-          }
-          else if(checktie(spaces)) {
-               cout << "\n\nDo you want to play again?(Y-Yes N-No)\n";
-               cin >> again;
-               if(again=='n' || again=='N') {
-                    running=false;
-                    break;
-               }
-               else{
-                    cleanboard(spaces);
-                    running=true;
-               }
-          }
-          aimove(spaces, ai);
-          board(spaces);
-          if(checkwinner(spaces, player, ai)) {
-               cout << "\n\nDo you want to play again?(Y-Yes N-No)\n";
-               cin >> again;
-               if(again=='n' || again=='N') {
-                    running=false;
-                    break;
-               }
-               else{
-                    cleanboard(spaces);
-                    running=true;
-               }
-          }
-          else if(checktie(spaces)) {
-               cout << "\n\nDo you want to play again?(Y-Yes N-No)\n";
-               cin >> again;
-               if(again=='n' || again=='N') {
-                    running=false;
-                    break;
-               }
-               else{
-                    cleanboard(spaces);
-                    running=true;
-               }
-          }
-          
-     }
-     return 0;
-}
-bool checkwinner(char* spaces, char player, char ai){
-     if((spaces[0]!=' ') && (spaces[0]==spaces[1] && spaces[0]==spaces[2])) {
-          spaces[0]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[0]!=' ') && (spaces[0]==spaces[3] && spaces[0]==spaces[6])) {
-          spaces[0]== player ? cout << "You win!\n" : cout << "You lose!\n";
-}
-     else if((spaces[0]!=' ') && (spaces[0]==spaces[4] && spaces[0]==spaces[8])) {
-          spaces[0]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[1]!=' ') && (spaces[1]==spaces[4] && spaces[1]==spaces[7])) {
-          spaces[1]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[2]!=' ') && (spaces[2]==spaces[4] && spaces[2]==spaces[6])) {
-          spaces[2]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[2]!=' ') && (spaces[2]==spaces[5] && spaces[2]==spaces[8])) {
-          spaces[2]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[3]!=' ') && (spaces[3]==spaces[4] && spaces[3]==spaces[5])) {
-          spaces[3]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     }
-     else if((spaces[6]!=' ') && (spaces[6]==spaces[7] && spaces[6]==spaces[8])) {
-          spaces[6]== player ? cout << "You win!\n" : cout << "You lose!\n";
-     } 
-     }
 
-bool checktie(char* spaces) {
-     for(int i=0; i<9; i++) {
-          if(spaces[i]==' ') {
-               return false;
-          }
-     }
-     cout << "The game is tie.\n";
-     return true;
+
+bool checkWinner(char* board, char player, char ai);
+bool checkTie(char* board);
+void playerMove(char* board);
+void aiMove(char* board, char ai, char player);
+void displayBoard(char* board);
+void cleanBoard(char* board);
+
+int main() {
+    char board[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+    char player = 'X';
+    char ai = 'O';
+    char again;
+    bool running = true;
+
+    while (running) {
+        displayBoard(board);
+        playerMove(board);
+        displayBoard(board);
+
+        if (checkWinner(board, player, ai)) {
+            cout << "\nDo you want to play again? (Y-Yes N-No): ";
+            cin >> again;
+            if (again == 'n' || again == 'N') running = false;
+            else cleanBoard(board);
+            continue;
+        }
+        if (checkTie(board)) {
+            cout << "\nDo you want to play again? (Y-Yes N-No): ";
+            cin >> again;
+            if (again == 'n' || again == 'N') running = false;
+            else cleanBoard(board);
+            continue;
+        }
+
+        aiMove(board, ai, player);
+        displayBoard(board);
+
+        if (checkWinner(board, player, ai)) {
+            cout << "\nDo you want to play again? (Y-Yes N-No): ";
+            cin >> again;
+            if (again == 'n' || again == 'N') running = false;
+            else cleanBoard(board);
+            continue;
+        }
+        if (checkTie(board)) {
+            cout << "\nDo you want to play again? (Y-Yes N-No): ";
+            cin >> again;
+            if (again == 'n' || again == 'N') running = false;
+            else cleanBoard(board);
+            continue;
+        }
+    }
+
+    return 0;
 }
-void playermove(char* spaces, char player) {
-     int number;
-     do{ 
-          cout << "Which spaces do you wanna choice(1-9)?:\n";
-          cin >> number;
-          number--;
-     if(spaces[number]==' ') {
-          spaces[number]=player;
-          break;
-     }
-     else {
-          cout << "Which spaces do you wanna choice(1-9)?:\n";
-          cin >> number;
-     }
-     }while((number>=0 && number<=8) || (spaces[number]==' '));
+
+
+bool checkWinner(char* board, char player, char ai) {
+    int wins[8][3] = {
+        {0,1,2},{3,4,5},{6,7,8},
+        {0,3,6},{1,4,7},{2,5,8}, 
+        {0,4,8},{2,4,6}           
+    };
+
+    for (int i = 0; i < 8; i++) {
+        if (board[wins[i][0]] != ' ' &&
+            board[wins[i][0]] == board[wins[i][1]] &&
+            board[wins[i][0]] == board[wins[i][2]]) {
+            board[wins[i][0]] == player ? cout << "You win!\n" : cout << "You lose!\n";
+            return true;
+        }
+    }
+    return false;
 }
-void board(char* spaces) {
-cout << endl;
-    cout << "      |      |      " << endl;
-    cout << "   " << spaces[0] << "  |   " << spaces[1] << "  |   " << spaces[2] << "   " << endl;
-    cout << "______|______|______" << endl;
-    cout << "      |      |      " << endl;
-    cout << "   " << spaces[3] << "  |   " << spaces[4] << "  |   " << spaces[5] << "   " << endl;
-    cout << "______|______|______" << endl;
-    cout << "      |      |      " << endl;
-    cout << "   " << spaces[6] << "  |   " << spaces[7] << "  |   " << spaces[8] << "   " << endl;
-    cout << "      |      |      " << endl;
+
+
+bool checkTie(char* board) {
+    for (int i = 0; i < 9; i++)
+        if (board[i] == ' ') return false;
+
+    cout << "The game is tie.\n";
+    return true;
+}
+
+
+void playerMove(char* board) {
+    int move;
+    while (true) {
+        cout << "Choose a position (1-9): ";
+        cin >> move;
+
+        if (cin.fail()) { 
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input. Enter a number 1-9.\n";
+            continue;
+        }
+
+        move--; 
+        if (move >= 0 && move < 9 && board[move] == ' ') {
+            board[move] = 'X';
+            break;
+        }
+        cout << "Invalid move. Try again.\n";
+    }
+}
+
+
+void displayBoard(char* board) {
+    cout << endl;
+    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << " \n";
+    cout << "---+---+---\n";
+    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << " \n";
+    cout << "---+---+---\n";
+    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << " \n";
     cout << endl;
 }
-void aimove(char* spaces, char ai) {
-     ai='O';
-     char player='X';
-     for(int i=0; i<=8; i++) {
-          if(spaces[i]==' ') {
-               spaces[i]=ai;
-               if(checkwinner(spaces, player, ai)) {
-                    return;
-          }
-          spaces[i]=' ';
-          }
-     }
-     for(int i=0; i<=8; i++) {
-          if(spaces[i]==' ') {
-               spaces[i]=player;
-               if(checkwinner(spaces, player, ai)) {
-                    spaces[i]=player;
-                    return;
-          }
-          spaces[i]=' ';
-          }
-     }
-     if(spaces[4]==' ') {
-          spaces[4]=ai;
-          return;
-     }
-     if(spaces[0]==player && spaces[8]==' ') {
-          spaces[8]=ai;
-          return;
-     }
-     if(spaces[2]==player && spaces[6]==' ') {
-          spaces[6]=ai;
-          return;
-     }
-     if(spaces[6]==player && spaces[2]==' ') {
-          spaces[2]=ai;
-          return;
-     }
-     if(spaces[8]==player && spaces[0]==' ') {
-          spaces[0]=ai;
-          return;
-     }
-     for(int i=0; i<=8; i++) {
-          if(spaces[i]==' ') {
-               spaces[i]=ai;
-               return;
-          }
-     }
-     
 
 
+void aiMove(char* board, char ai, char player) {
+ 
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ') {
+            board[i] = ai;
+            if (checkWinner(board, player, ai)) return;
+            board[i] = ' ';
+        }
+    }
+    
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ') {
+            board[i] = player;
+            if (checkWinner(board, player, ai)) {
+                board[i] = ai;
+                return;
+            }
+            board[i] = ' ';
+        }
+    }
+    
+    if (board[4] == ' ') { board[4] = ai; return; }
+
+    
+    int positions[9] = {0,2,6,8,1,3,5,7};
+    for (int i = 0; i < 9; i++) {
+        if (board[positions[i]] == ' ') {
+            board[positions[i]] = ai;
+            return;
+        }
+    }
 }
-void cleanboard(char* spaces) {
-     for(int i=0; i<9; i++) {
-          spaces[i]=' ';
-     }
+
+
+void cleanBoard(char* board) {
+    for (int i = 0; i < 9; i++) board[i] = ' ';
 }
